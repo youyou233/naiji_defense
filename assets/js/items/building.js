255155292cbd2a900d83d6fa3b2f3a8a7d3c0633
ielgned <321041567@qq.com>
2019-06-26 18:38:50 +0800

    修改

--- assets/js/items/building.js
+++ assets/js/items/building.js
@@ -1,32 +1,33 @@
 cc.Class({
-  extends: cc.Component,
+	extends: cc.Component,
+// TODO: 
+	properties: {
+		building: null,
+		type: 0, //0表示无建筑
+	},
 
-  properties: {
-    building: null,
-    type:0,//0表示无建筑
-  },
-
-  // 一轮游戏的初始化
-  init(g, num) { //num从1开始传递
-    this._game = g
-    this.loadChild().then(() => {
-      this.buildingSprite = num ? g.buildingSF[num - 1] : null
-      this.menuNode.active = num ? false : true
-    })
-  },
-  loadChild() {
-    return new Promise((resolve) => {
-      this.buildingSprite = this.node.getChildByName('building').getComponent(cc.Sprite)
-      this.menuNode = this.node.getChildByName('btn')
-    })
-  },
-  clickBuildingBtn() {
-    // 打开弹框
-    this._game.clickBuildingBtn(this)
-  },
-  changeBuilding(num) {
-    this.buildingSprite.spriteFrame = this._game.buildingSF[num - 1]
-    this.menuNode.active = false
-  }
-  // update (dt) {},
+	// 一轮游戏的初始化
+	init(g, num) { //num从1开始传递
+		this._game = g
+		this.type = num
+		this.loadChild().then(() => {
+			this.buildingSprite = num ? g.buildingSF[num - 1] : null
+			this.menuNode.active = num ? false : true
+		})
+	},
+	loadChild() {
+		return new Promise((resolve) => {
+			this.buildingSprite = this.node.getChildByName('building').getComponent(cc.Sprite)
+			this.menuNode = this.node.getChildByName('btn')
+		})
+	},
+	clickBuildingBtn() {
+		// 打开弹框
+		this._game.clickBuildingBtn(this)
+	},
+	changeBuilding(num) {
+		this.buildingSprite.spriteFrame = this._game.buildingSF[num - 1]
+		this.menuNode.active = false
+	}
+	// update (dt) {},
 });
---
+++ assets/js/items/naiji.js
@@ -0,0 +1,16 @@
+cc.Class({
+  extends: cc.Component,
+
+  properties: {},
+
+  // LIFE-CYCLE CALLBACKS:
+
+  // onLoad () {},
+  init(c) {
+    this._controller = c
+    this.loadChild()
+  },
+  loadChild() {
+  },
+
+});
